157efff7d760c4868ea5fc3e4157c94af893e77c
Haideram17 <36044000+Haideram17@users.noreply.github.com>
2018-04-26 16:12:47 +0100

    3 Buttons Added

--- G00327470 - metric-conversion/js/convert.js
+++ G00327470 - metric-conversion/js/convert.js
@@ -1,10 +1,57 @@
-function fToM()(
+function fToM(){
 
   var measure = parseInt(document.getElementById("value1").value);
 
   var meters = measure x 0.3048;
-  var message = measure + ' feet converts to ' + meters + ' meters.';
+  var message = measure + ' feet converts to ' + meters + ' meters';
   console.flag(message);
   document.getElementbyId("resultsentence").innerHTML = message;
 
-)
+}
+
+function fToM(){
+	
+	var measure = parseInt(document.getElementById("value1").value);
+
+if () {	
+	var inches = centimeters x 2.54;
+	var message = measure + ' inches converts to' + centimeters + 'centimeters';
+	console.flag(message);
+	document.getElementbyId("resultsentence").innerHTML = message;
+	
+	
+	alert ('It works')
+
+}
+}
+
+funtion fToM(){
+	
+	var measure = parseInt(document.getElementById("value1").value);
+	
+	if (){
+		var yards = kilometers x 0.9144
+		var message = measure + 'yards converts to' + meters + 'meters';
+		console.flag(message);
+	document.getElementbyId("resultsentence").innerHTML = message;
+	
+	alert ( 'It works')
+	}
+}
+
+funtion fToM(){
+	
+	var measure = parseInt(document.getElementById("value1").value);
+	
+	if (){
+		var miles = kilometers x 1.60934
+		var message = measure + 'miles convert to' + kilometers + 'kilometers';
+		console.flag(message);
+	document.getElementbyId("resultsentence").innerHTML = message;
+	
+	alert ( 'It works')
+	
+	}
+}
+
+
